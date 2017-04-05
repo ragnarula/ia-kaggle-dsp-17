5b4f8529ffe58f50315d4f71f73c3a10c12bf126
Raghav Narula <hello@raghavnarula.co.uk>
2017-04-05 17:16:47 +0100

    added more to model

--- config.py
+++ config.py
@@ -44,16 +44,19 @@ def get_model(input_shape, **kwargs):
     model = Sequential()
 
     model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape, data_format='channels_last'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(32, (1, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(64, (1, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(128, (1, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
