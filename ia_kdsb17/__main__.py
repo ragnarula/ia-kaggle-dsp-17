d4e2dcfcdfc34c98fc72a156856c343f34001fd9
Raghav Narula <hello@raghavnarula.co.uk>
2017-04-05 09:34:21 +0100

    added gpu requirements

--- ia_kdsb17/__main__.py
+++ ia_kdsb17/__main__.py
@@ -43,7 +43,7 @@ model.add(Dense(2, activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='sparse_categorical_crossentropy', optimizer=sgd)
 
-model.fit(train_data[0:4, :, :], labels[0:4], batch_size=1, epochs=10)
+model.fit(train_data[0:4, :, :], labels[0:4], batch_size=128, epochs=10)
 
 predictions = model.predict(train_data[4:8, :, :])
 
